e21b87d458b70eadbb34aa15670d755c43d6b372
TofaelislamFaruk01 <tofaelfaruk@gmail.com>
2023-09-24 01:57:01 +0600

    login file and login ja file complete

---
+++ js/login.js
@@ -0,0 +1,16 @@
+document.getElementById('submit_btn').addEventListener('click',function () {
+    console.log(7);
+
+    const emailBtn = document.getElementById('email_btn');
+    const passBtn = document.getElementById('pass_btn');
+    if (emailBtn.value === 'rana' && passBtn.value === '1234')
+    {
+        console.log('login success');
+        window.location.href = 'bank.html';
+    }
+    else {
+        alert('Invalid username or password');
+        emailBtn.value = '';
+        passBtn.value = '';
+    }
+})
